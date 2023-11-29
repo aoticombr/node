f5a2dd131e31bcbfe9e21cb16529912d45e3b22f
krobrelus <paulo@aoti.com.br>
2023-11-29 19:23:06 -0400

    first

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ import post from './lib/post';
 import str from './lib/str';
 import uuid from './lib/uuid';
 import token from './middleware/token';
+import jwt from 'jsonwebtoken';
 
 export {
+    jwt,
     date,
     db,
     json,
